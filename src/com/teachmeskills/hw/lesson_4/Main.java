951efce464dcd9f7ee8a774d614590808206a1e0
Marharyta Amosava <mozheliukrita@gmail.com>
2024-10-09 20:24:21 +0200

    Task number 0 is completed with Main class

--- src/com/teachmeskills/hw/lesson_4/Main.java
+++ src/com/teachmeskills/hw/lesson_4/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Task number 0 is launched.");
+        TaskNumber0.TaskNumber0Runner();
     }
 }
--- src/com/teachmeskills/hw/lesson_4/TaskNumber0.java
+++ src/com/teachmeskills/hw/lesson_4/TaskNumber0.java
@@ -1,4 +1,33 @@
 package com.teachmeskills.hw.lesson_4;
 
+import java.util.Scanner;
+
 public class TaskNumber0 {
+    /*0. Создайте массив целых чисел. Напишете программу, которая выводит
+    сообщение о том, входит ли заданное число в массив или нет.
+    Пусть число для поиска задается с консоли (класс Scanner). */
+    
+    public static void TaskNumber0Runner(){
+        int[] integerArray = {14, -5, -123, -34, 44, 6, 87, 45, 35, 3};
+        System.out.println("Please, enter a number: ");
+        Scanner input = new Scanner(System.in);
+        int userInput = input.nextInt();
+        boolean isArrayContainsEnteredNumber = false;
+
+        for (int i = 0; i < integerArray.length; i++){
+            if (userInput == integerArray[i])
+            {
+                isArrayContainsEnteredNumber = true;
+                break;
+            }
+        }
+
+        if (isArrayContainsEnteredNumber)
+        {
+            System.out.println("The array contains the entered number");
+        }
+        else {
+            System.out.println("The array doesn't contain the entered number");
+        }
+    }
 }
